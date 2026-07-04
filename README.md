d13d17d28f619ffa021117108fcd96f408a4fdf9
Wali Johnson <waliproductionsllc@gmail.com>
2026-07-03 23:46:52 -0500

    Fix live branch with complete production build

--- README.md
+++
@@ -1,9 +0,0 @@
-# Wali Studios Live Deployment
-
-This branch contains only the built static website files for Hostinger Git deployment.
-
-Do not develop directly on this branch.
-
-Source code lives on:
-- main
-- development
---
+++ assets/disc-3-Bu_-V50w.js
@@ -0,0 +1,11 @@
+import{c}from"./index-SftpaMEq.js";/**
+ * @license lucide-react v0.469.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=c("CirclePlay",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["polygon",{points:"10 8 16 12 10 16 10 8",key:"1cimsy"}]]);/**
+ * @license lucide-react v0.469.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=c("Disc3",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M6 12c0-1.7.7-3.2 1.8-4.2",key:"oqkarx"}],["circle",{cx:"12",cy:"12",r:"2",key:"1c9p78"}],["path",{d:"M18 12c0 1.7-.7 3.2-1.8 4.2",key:"1eah9h"}]]);export{y as C,r as D};
